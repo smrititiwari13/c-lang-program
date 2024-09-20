efbc862531bb07705a3abab52a92fdc53c3ecaf8
smrititiwari13 <smrititiwari1313@gmail.com>
2024-09-20 12:08:24 +0530

    Add files via upload

---
+++ THIRD.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ clrscr();
+ int a=2,l=3,c=2;
+ float d=3.14;
+ printf("area of squre %d\n",a*a);
+ printf("perimeter of square %d\n",4*a);
+ printf("area of ractangle %d\n",a*l);
+ printf("perimeter of ract %d\n",2*(l+a));
+ printf(" area of circle %f\n",d*c*c);
+ printf("perimeter of circlr %f\n",2*d*c);
+ getch();
+}

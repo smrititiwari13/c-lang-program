ffabb4ce9af0616bb06086e77b04c019eaf4a839
smrititiwari13 <smrititiwari1313@gmail.com>
2024-09-25 12:30:25 +0530

    Add files via upload

---
+++ EVENODD.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a;
+	clrscr();
+	printf("enter any number\n");
+	scanf("%d",&a);
+	if(a%2==0)
+		printf("number is even");
+
+	else
+		printf("number is odd");
+
+	getch();
+
+}

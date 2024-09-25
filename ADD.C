c600d0864225b444d22c86518bba7b5121db250a
smrititiwari13 <smrititiwari1313@gmail.com>
2024-09-25 12:11:17 +0530

    Add files via upload

---
+++ ADD.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a,b;
+	clrscr();
+	printf("enter first value\n");
+	scanf("%d",&a);
+
+	printf("enter second value\n");
+	scanf("%d",&b);
+
+	printf("sum of numbers=%d",a+b);
+	getch();
+}
+

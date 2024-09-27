58afec00abfa5f89369a435766d49c1b9032cd57
smrititiwari13 <smrititiwari1313@gmail.com>
2024-09-27 11:52:27 +0530

    Add files via upload

---
+++ SIZEOF.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	clrscr();
+	int i;
+	float f;
+	char c;
+	double d;
+	printf("size of int:%lu\n",sizeof(i));
+	printf("size of float:%lu\n",sizeof(f));
+	printf("size of char:%lu\n",sizeof(c));
+	printf("size of double:%lu\n",sizeof(d));
+	getch();
+}

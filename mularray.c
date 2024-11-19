ad64d5a68afee3aac14fddbf14870f9893c5c5c3
smrititiwari13 <smrititiwari1313@gmail.com>
2024-11-19 15:34:46 +0530

    Add files via upload

---
+++ mularray.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{
+	int a[2][2],b[2][2],c[2][2],i,j,k;
+	printf("enter first matrix elemaent\n");
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("enter 2nd matrix element\n");
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+		 scanf("%d",&b[i][j]);	
+		}
+	}
+	
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			c[i][j]=0;
+			for(k=0;k<2;k++)
+			{
+				c[i][j]+=a[i][k]*b[k][j];
+			}
+		}
+	}
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			printf("%d ",c[i][j]);
+			
+		}
+		printf("\n");
+	}
+	return 0;
+}

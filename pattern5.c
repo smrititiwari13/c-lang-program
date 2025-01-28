792cdf06756512dbdefca8ef5366b4f4389fb8af
smrititiwari13 <smrititiwari1313@gmail.com>
2025-01-28 12:26:37 +0530

    Create pattern5.c

---
+++ pattern5.c
@@ -0,0 +1,17 @@
+//alphabeticle pyramid half
+#include <stdio.h>
+
+int main()
+{
+   int i,j;
+   for(i=65;i<=70;i++)
+   {
+       for(j=65;j<=i;j++)
+       {
+           printf("%c",j);
+       }
+       printf("\n");
+   }
+    
+    return 0;
+}

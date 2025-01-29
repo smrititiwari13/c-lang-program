4785e98ed69146f33e6ec81e247b98b21ffcc924
smrititiwari13 <smrititiwari1313@gmail.com>
2025-01-29 10:25:37 +0530

    Create fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+int fact(int n)
+{
+   if(n==0)
+    return 0;
+    else if(n==1)
+    return 1; 
+    else
+    return fact(n-1)+fact(n-2);
+    
+}
+
+int main()
+{
+    int n;
+    printf("enter number");
+    scanf("%d",&n);
+    fact(n);
+    for(int i=0;i<=n;i++)
+    printf("%d",fact(i));
+    return 0;
+}

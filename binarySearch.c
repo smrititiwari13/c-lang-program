9d8353b2234144e2860f2c69dfd0ab125286af15
smrititiwari13 <smrititiwari1313@gmail.com>
2025-01-30 10:10:51 +0530

    Create binarySearch.c

---
+++ binarySearch.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int BinarySearch(int a[],int low,int high, int x)
+{
+  while(low<=high)
+  {
+      int mid=(low+high)/2;
+      if(a[mid]==x)
+      return mid;
+      else if(a[mid]<x)
+      low=mid+1;
+      else
+      high=mid-1;
+  }
+  return -1;
+    
+    
+}
+int main()
+{
+    int x;
+    int a[]={10,20,30,40,50,60};
+    int n=sizeof(a)/sizeof(a[0]);
+    printf("enter element to search");
+    scanf("%d",&x);
+    int result=BinarySearch(a,0,n-1,x);
+    if(result==-1)
+    printf("element not present");
+    else
+    printf("element found at index %d",result);
+    
+    return 0;
+}

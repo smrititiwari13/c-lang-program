80da21c91d3e60539555a0881dc4b278d8701edf
smrititiwari13 <smrititiwari1313@gmail.com>
2025-01-30 10:40:04 +0530

    Create gcdrecu.c

---
+++ gcdrecu.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+int gcd(int a,int b)
+{
+    if(b==0)
+    return a;
+    else
+    return gcd(b,a%b);
+    
+}
+
+int main()
+{
+    int a,b;
+    scanf("%d,%d",&a,&b);
+    printf("gcd=%d",gcd(a,b));
+ 
+    return 0;
+}

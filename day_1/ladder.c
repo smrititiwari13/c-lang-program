90a102f0a2bcee49de063244f8fda9c82f03fb94
smrititiwari13 <smrititiwari1313@gmail.com>
2025-02-08 19:43:09 +0530

    Create ladder.c

---
+++ day_1/ladder.c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+
+int main() 
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+   {
+       // for(int j=0;j<=i-1;j++)
+      // printf(" ");
+       for(int j=0;j<i+1;j++)
+       printf("* ");
+      
+       printf("\n");
+   }
+   
+   for(int i=0;i<n;i++)
+   {
+       for(int j=0;j<n;j++)
+       printf("  ");
+      for(int j=0;j<i+1;j++)
+        printf("* ");
+    
+       printf("\n");
+   }
+    for(int i=0;i<n;i++)
+   {
+       for(int j=0;j<n;j++)
+       printf("    ");
+      for(int j=0;j<i+1;j++)
+        printf("* ");
+    
+       printf("\n");
+   }
+   
+
+    return 0;
+}

496831ca56a6b74ace5e55b6bdb7fa485456b8b6
smrititiwari13 <smrititiwari1313@gmail.com>
2025-02-08 19:26:53 +0530

    Update leftArrow2.0.c

--- day_1/leftArrow2.0.c
+++ day_1/leftArrow2.0.c
@@ -5,9 +5,9 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    for(int i=n-1;i>1;i--)
+    for(int i=n-1;i>=1;i--)
    {
-        for(int j=0;j<i-1;j++)
+        for(int j=0;j<=i-1;j++)
        printf(" ");
        for(int j=0;j<=i;j++)
        printf("*");
